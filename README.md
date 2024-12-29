bcee1813b1173a561b80efcdf18456aa69f9fb04
xiehaiyong <xietfsj@163.com>
2024-12-29 21:10:22 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# Development
+
+Your new jumpstart project includes basic organization with an organized `assets` folder and a `components` folder. 
+If you chose to develop with the router feature, you will also have a `views` folder.
+
+### Tailwind
+1. Install npm: https://docs.npmjs.com/downloading-and-installing-node-js-and-npm
+2. Install the Tailwind CSS CLI: https://tailwindcss.com/docs/installation
+3. Run the following command in the root of the project to start the Tailwind CSS compiler:
+
+```bash
+npx tailwindcss -i ./input.css -o ./assets/tailwind.css --watch
+```
+
+### Serving Your App
+
+Run the following command in the root of your project to start developing with the default platform:
+
+```bash
+dx serve --platform web
+```
+
+To run for a different platform, use the `--platform platform` flag. E.g.
+```bash
+dx serve --platform desktop
+```
+
+## Usage
+
+- To run it in your localhost browser
+```
+cd dioxus-ssg/
+
+dx serve --platform web --open
+```
+
+- To deploy using static site generation (SSG)
+```
+cd dioxus-ssg/
+
+rm -rf dist/
+
+rm -rf static/
+
+rm -rf target/dx/
+
+dx build --release --ssg
+
+mkdir dist/
+
+cp -r target/dx/dioxus-ssg/release/web/public/* dist/
+
+cp -r static/* dist/
+
+cd dist/
+
+http-server -c-1 -o
+```
---
+++ src/main.rs
@@ -0,0 +1,61 @@
+use dioxus::prelude::*;
+
+use components::Navbar;
+use views::{Blog, Home};
+
+mod components;
+mod views;
+
+#[derive(Debug, Clone, Routable, PartialEq)]
+#[rustfmt::skip]
+enum Route {
+    #[layout(Navbar)]
+    #[route("/")]
+    Home {},
+    #[route("/blog/:id")]
+    Blog { id: i32 },
+}
+
+const FAVICON: Asset = asset!("/assets/favicon.ico");
+const MAIN_CSS: Asset = asset!("/assets/styling/main.css");
+const TAILWIND_CSS: Asset = asset!("/assets/tailwind.css");
+
+
+#[server(endpoint = "static_routes")]
+async fn static_routes() -> Result<Vec<String>, ServerFnError> {
+  Ok(
+    Route::static_routes()
+      .into_iter()
+      .map(|route| route.to_string())
+      .collect::<Vec<_>>(),
+  )
+}
+
+
+fn main() {
+
+    LaunchBuilder::new()
+    .with_cfg(server_only! {
+      ServeConfig::builder()
+        // turn on incremental site generation with the .incremental() method
+        .incremental(IncrementalRendererConfig::new())
+        .build()
+        .unwrap()
+    })
+    .launch(App);
+    // dioxus::launch(App);
+}
+
+#[component]
+fn App() -> Element {
+    // Build cool things ✌️
+
+    rsx! {
+        // Global app resources
+        document::Link { rel: "icon", href: FAVICON }
+        document::Link { rel: "stylesheet", href: MAIN_CSS }
+        document::Link { rel: "stylesheet", href: TAILWIND_CSS }
+
+        Router::<Route> {}
+    }
+}
